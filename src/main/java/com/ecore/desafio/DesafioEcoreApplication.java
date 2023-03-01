c18ba611ef749bfdfe93db397f9c776eee564bbd
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-03-01 14:29:25 -0300

    First commit

---
+++ src/main/java/com/ecore/desafio/DesafioEcoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecore.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEcoreApplication.class, args);
+	}
+
+}
