65c2ca81b53dc74c22fe1a2dbb1ba2fdc96e359b
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-03-01 21:29:40 -0300

    Added JPA, H2 and Entinty Role

--- src/main/java/com/ecore/desafio/model/Role.java
+++ src/main/java/com/ecore/desafio/model/Role.java
@@ -1,12 +1,23 @@
 package com.ecore.desafio.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Role {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
 	private String name;
 	
 	public Role() {}
 
-	public Role(String name) {
+	public Role(Long id, String name) {
+		super();
+		Id = id;
 		this.name = name;
 	}
 
@@ -17,4 +28,14 @@ public class Role {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+	
+	
 }
--- src/main/java/com/ecore/desafio/services/RoleService.java
+++ src/main/java/com/ecore/desafio/services/RoleService.java
@@ -1,13 +1,11 @@
 package com.ecore.desafio.services;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.ecore.desafio.model.Role;
-import com.ecore.desafio.util.Util;
 
 @Service
 public class RoleService {
@@ -16,17 +14,17 @@ public class RoleService {
 		
 		List<Role> roles = new ArrayList<>();
 				
-		for (String n : Util.exitingRoles) {
-			Role role = new Role(n);
-			roles.add(role);
-		}
-		
-		Role role = new Role(name);
-		
-		if (!roles.contains(role)) {
-			roles.add(role);
-			Util.exitingRoles[Util.exitingRoles.length+1] = role.getName();
-		}
+//		for (String n : Util.exitingRoles) {
+//			Role role = new Role(n);
+//			roles.add(role);
+//		}
+//		
+//		Role role = new Role(name);
+//		
+//		if (!roles.contains(role)) {
+//			roles.add(role);
+//			Util.exitingRoles[Util.exitingRoles.length+1] = role.getName();
+//		}
 	
 		return roles;
 	}
@@ -34,10 +32,10 @@ public class RoleService {
 	public List<Role> getRoles() {
 		List<Role> roles = new ArrayList<>();
 		
-		for (String n : Util.exitingRoles) {
-			Role role = new Role(n);
-			roles.add(role);
-		}
+//		for (String n : Util.exitingRoles) {
+//			Role role = new Role(n);
+//			roles.add(role);
+//		}
 		
 		return roles;
 	}
